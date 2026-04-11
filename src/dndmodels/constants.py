68de178db24f483bf936ad229f997ffcb06c0322
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2026-04-11 16:54:14 -0700

    New model design. Basics implemented. Ready for test writing.

--- src/dndmodels/constants.py
+++ src/dndmodels/constants.py
@@ -1,6 +1,28 @@
 from enum import StrEnum
 
 
+class BaseStat(StrEnum):
+    STRENGTH = "strength"
+    DEXTERITY = "dexterity"
+    CONSTITUTION = "constitution"
+    INTELLIGENCE = "intelligence"
+    WISDOM = "wisdom"
+    CHARISMA = "charisma"
+
+class AttackEvent(StrEnum):
+    EVENT_TYPE = "event_type"
+    NUM_TARGETS = "num_targets"
+    DAMAGE_CODE = "damage_code"
+    DAMAGE_TYPE = "damage_type"
+    HIT_BONUS = "hit_bonus"
+    SAVE_DC = "save_dc"
+    SAVE_TYPE = "save_type"
+
+class SaveEffect(StrEnum):
+    HALF_DAMAGE = "half_damage"
+    NEGATE_FULL = "negate_full"
+    NEGATE_PARTIAL = "negate_partial"
+
 class Situation(StrEnum):
     NORMAL = "Normal"
     ADVANTAGE = "Advantage"
